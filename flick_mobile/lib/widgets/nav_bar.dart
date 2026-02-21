05def701ef98713888ab147a8383e29bcc11aff2
marscp <marscpwork@gmail.com>
2026-02-21 09:21:05 +0100

    fix: Corrección de bug en el Nav Bar

--- flick_mobile/lib/widgets/nav_bar.dart
+++ flick_mobile/lib/widgets/nav_bar.dart
@@ -9,33 +9,31 @@ class NavBar extends StatelessWidget {
     final tx = Theme.of(context).textTheme;
     final clr = Theme.of(context).colorScheme;
 
-    return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        color: clr.primary,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/home');
-                },
-                icon: Icon(Icons.home, size: 30),
-              ),
-              IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/search');
-                },
-                icon: Icon(Icons.search, size: 30),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.library_books, size: 30),
-              ),
-              IconButton(onPressed: () {}, icon: Icon(Icons.person, size: 30)),
-            ],
-          ),
+    return BottomAppBar(
+      color: clr.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/home');
+              },
+              icon: Icon(Icons.home, size: 30),
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/search');
+              },
+              icon: Icon(Icons.search, size: 30),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.library_books, size: 30),
+            ),
+            IconButton(onPressed: () {}, icon: Icon(Icons.person, size: 30)),
+          ],
         ),
       ),
     );
